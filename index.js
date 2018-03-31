762b0d677fdff361f2055acddd1a7c49d3fec568
acherone92@gmail.com <acherone92@gmail.com>
2018-03-31 20:10:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector(".target");
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('.ranked-list li');
+  for (let i=0; i<lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+    console.log(lis[i]);
+  }
+}
+
+
+function deepestChild(){
+  const gN = document.getElementById('#grand-node').getElementsByTagName('div');
+  for (i=0; i<gN.length; i++){
+    while (typeof gN[i].innerHTML=== Array){
+      
+    }
+  }
+}
